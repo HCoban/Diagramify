104ce7e0487827973d59356ee2c75444d4c98023
Halil Coban <halil.coban@gmail.com>
2016-09-19 14:34:41 -0700

    added ar

--- lib/diagramify.rb
+++ lib/diagramify.rb
@@ -1,5 +1,11 @@
 require "diagramify/version"
 require "diagramify/model"
+require "rails"
+require "active_support"
+require "rubygems"
+require "active_record"
+
+debugger
 
 # Rails.application.eager_load!
 
